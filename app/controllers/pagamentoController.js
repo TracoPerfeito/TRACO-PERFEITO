22c73aa7d33ce425d67f3a6a1d053d6a528543b8
Ana Julia <tracoperfeito2024@outlook.com>
2025-09-23 19:59:14 -0300

    ju

--- app/controllers/pagamentoController.js
+++ app/controllers/pagamentoController.js
@@ -4,36 +4,36 @@ const moment = require("moment");
 
 const pagamentoController = {
 
-    gravarPagamento: async (req, res) => {
-        console.log("Registrando pagamento...");
-        console.log("Recebido no feedback:", req.body, req.query);
+   gravarPagamento: async (req, res) => {
+    console.log("Registrando pagamento...");
+    console.log("Recebido no feedback:", req.body, req.query);
 
-        try {
+    try {
+        const idPedido = req.query.external_reference; 
+        const camposJsonPagamento = {
+            ID_PEDIDO: idPedido,
+            ID_USUARIO: req.session.autenticado.id,
+            METODO: req.query.payment_type,
+            VALOR: req.query.preference_id, 
+            STATUS: req.query.status || "pendente",
+            DATA: moment().format("YYYY-MM-DD HH:mm:ss")
+        };
 
+        console.log("Dados do pagamento a serem registrados:", camposJsonPagamento);
 
-            const camposJsonPagamento = {
-                ID_PEDIDO: req.body.id_pedido,               // vincula com o pedido
-                ID_USUARIO: req.session.autenticado.id,      // usuário logado
-                METODO: req.body.metodo,                     // "Cartão", "PIX", etc.
-                VALOR: req.body.valor,                       // total
-                STATUS: req.body.status || "pendente", 
-                DATA: moment().format("YYYY-MM-DD HH:mm:ss")
-            };
+        const create = await PagamentoModel.create(camposJsonPagamento);
 
-            console.log("Dados do pagamento a serem registrados:", camposJsonPagamento);
-            // cria registro na tabela PAGAMENTO
-            const create = await PagamentoModel.create(camposJsonPagamento);
-
-            console.log("Pagamento registrado com sucesso, ID:", create.insertId);
-            res.json({
-                mensagem: "Pagamento registrado com sucesso!",
-                id_pagamento: create.insertId
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ erro: "Erro ao registrar pagamento" });
-        }
+        console.log("Pagamento registrado com sucesso, ID:", create.insertId);
+        res.json({
+            mensagem: "Pagamento registrado com sucesso!",
+            id_pagamento: create.insertId
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ erro: "Erro ao registrar pagamento" });
     }
+}
+
 };
 
 module.exports =  pagamentoController ;
